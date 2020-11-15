df6c54f364264b0190600ab9d6acf179a70593f4
rishi0078 <39759671+rishi0078@users.noreply.github.com>
2020-11-15 17:17:41 +0530

    Update SearchPage.jsx

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -6,18 +6,25 @@ import Response from "./response"
 import { Link } from 'react-router-dom';
 import Search from "./pages/Search";
 import "./pages/Search.css";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 
 
 
 function SearchPage() {
     const [{term},dispatch]=useStateValue();
-    //const{ data }=useGoogleSearch(term);
+  // const{ data }=useGoogleSearch(term);
     //LIVE API CALL
-    // const {data}=useGoogleSearch(term);
+    const {data}=useGoogleSearch(term);
 
     // Mock API CALL
-     const data = Response;
+    // const data = Respone;
+    
 
 
    console.log(data)
@@ -25,16 +32,14 @@ function SearchPage() {
      
         <div className='SearchPage'>
             <div className='searchPage_header'>
-               <Link to="/">
-                
-               </Link>
+               
 
                 <div className="searchPage_headerBody">
                     <Search hideButtons />
 
 
-                   
-
+                    <div className="searchPage_option">
+                     
                     </div>
 
                 </div>
