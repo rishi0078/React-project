77e1a2baaf2d32b5fc64ecd8d9f54639c4e8e521
rishi0078 <39759671+rishi0078@users.noreply.github.com>
2020-11-15 16:50:10 +0530

    Update SearchPage.jsx

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -6,12 +6,7 @@ import Response from "./response"
 import { Link } from 'react-router-dom';
 import Search from "./pages/Search";
 import "./pages/Search.css";
-import SearchIcon from "@material-ui/icons/Search";
-import DescriptionIcon from "@material-ui/icons/Description";
-import ImageIcon from "@material-ui/icons/Image";
-import LocalOfferIcon from "@material-ui/icons/LocalOffer";
-import RoomIcon from "@material-ui/icons/Room";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 
 
 
