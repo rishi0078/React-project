7936e6054996ea888b3884ea85411bc2bad9e358
rishi0078 <39759671+rishi0078@users.noreply.github.com>
2020-11-18 15:31:13 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,26 @@ import Search from './Search'
 
 function Home() {
     return (
-       
+       <div className='home'>
+             
+          <div className='home_header'>
+               <div className='home_headerLeft'>
+                  <Link to='/about'>About</Link>   {/* Link*/}
+                  <Link to='/Store'>Store</Link>   {/* Link*/}
+               </div>
+
+               <div className='home_headerRight'>
+                     
+                      <Link to='/Gmail'>Gmail</Link> 
+                      <Link to='/Images'>Images</Link> 
+                      <AppsIcon />
+                      <Avatar />
+
+               </div>
+          </div>   
           <div className='home_body'>
                
-           
+           <img src="https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
            <div className="home_inputContainer">
                {/* <Search /> */}
              <Search  />
